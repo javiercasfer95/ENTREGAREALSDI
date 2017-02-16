8f304df0a5aa352d3c386e95d35249cd14b2dee7
Javier C. f(z) <javitxu1995@gmail.com>
2017-02-16 10:27:33 +0100

    Registro de usuarios añadido

---
+++ TaskManager/src/uo/sdi/acciones/RegistrarUsuarioAction.java
@@ -0,0 +1,67 @@
+package uo.sdi.acciones;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import alb.util.log.Log;
+import uo.sdi.business.UserService;
+import uo.sdi.business.exception.BusinessException;
+import uo.sdi.business.impl.user.UserServiceImpl;
+import uo.sdi.dto.User;
+import uo.sdi.util.Comprobador;
+
+public class RegistrarUsuarioAction implements Accion {
+
+	/*
+	 * Dos opciones:
+	 * 
+	 * Lo guardamos en la base si tuvo éxito e iniciamos sesion.
+	 * 
+	 * Lo guardamos en la base y le mandamos al login para que inicie sesion.
+	 * (Esta será mas facil, es la que estoy usando actualmente.)
+	 * 
+	 * Mirar mapa de acciones del controlador.
+	 */
+
+	@Override
+	public String execute(HttpServletRequest request,
+			HttpServletResponse response) {
+		// Resultados que puede mandar
+		String result = "EXITO";
+
+		// Obtencion de datos
+
+		String nombreUsuario = request.getParameter("nombreUsuario");
+		String email = request.getParameter("email");
+		String contraseña = request.getParameter("pass");
+		String contraseñaRepetida = request.getParameter("pass2");
+
+		// HttpSession session = request.getSession();
+		User user = new User();
+
+		if (!Comprobador.esEmailValido(email)) {
+			// El email no es valido y hay que tratar el error
+			Log.debug("Email inválido!");
+			result = "FRACASO";
+		} else if (Comprobador.esContraseñasIguales(contraseña,
+				contraseñaRepetida)) {
+			Log.debug("Las contraseñas no son iguales!");
+			result = "FRACASO";
+		} else {
+			user.setEmail(email);
+			user.setPassword(contraseña);
+			user.setLogin(nombreUsuario);
+			UserService userService = new UserServiceImpl();
+			try {
+				userService.registerUser(user);
+			} catch (BusinessException e) {
+				Log.debug("El registro de usuario ha fallado por lo siguiente:\n"
+						+ e.getMessage());
+				result = "FRACASO";
+			}
+		}
+
+		return result;
+	}
+
+}
---
+++ TaskManager/src/uo/sdi/util/Comprobador.java
@@ -0,0 +1,29 @@
+package uo.sdi.util;
+
+public class Comprobador {
+	
+	public static boolean esEmailValido(String email){
+		try{
+			String[] cachos = email.split("@");
+			if(cachos.length != 2){
+				throw new Exception();
+			}
+			cachos[1].split(".");
+		}catch(Exception e){
+			return false;
+		}
+		return true;
+	}
+	
+	public static boolean esContraseñasIguales(String p1, String p2){
+		char[] pass1 = p1.toCharArray();
+		char[] pass2 = p2.toCharArray();
+		if(pass1.length != pass2.length)
+			return false;
+		for(int i = 0; i<pass1.length; i++){
+			if(pass1[i] != pass2[i])
+				return false;
+		}
+		return true;
+	}
+}
