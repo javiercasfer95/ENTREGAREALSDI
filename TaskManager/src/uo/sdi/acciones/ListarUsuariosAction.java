c3e8618e6c02b1c46928b58c91a1901d6bc1e45b
ivanpsl <ivanpsl95@gmail.com>
2017-02-20 18:34:16 +0100

    ListarUsuarios

---
+++ TaskManager/src/uo/sdi/acciones/ListarUsuariosAction.java
@@ -0,0 +1,50 @@
+package uo.sdi.acciones;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import uo.sdi.business.AdminService;
+import uo.sdi.business.Services;
+import uo.sdi.business.TaskService;
+import uo.sdi.business.exception.BusinessException;
+import uo.sdi.dto.Task;
+import uo.sdi.dto.User;
+import alb.util.log.Log;
+
+public class ListarUsuariosAction implements Accion{
+
+	@Override
+	public String execute(HttpServletRequest request,
+			HttpServletResponse response) {
+		String resultado = "EXITO";
+
+		List<User> listaUsuarios;
+		
+		HttpSession session = request.getSession();
+
+		User actualUser = (User) session.getAttribute("user");
+		Long idUser = actualUser.getId();
+		try {
+			AdminService adminService = Services.getAdminService();
+
+			//obtenemos todos los usuarios existentes
+			listaUsuarios = adminService.findAllUsers();
+		
+
+			Log.debug(
+					"Obtenida lista de usuarios. Obtenidos [%d] usuarios ",
+					listaUsuarios.size());
+			
+			
+		} catch (BusinessException b) {
+			Log.debug("Algo ha ocurrido obteniendo lista de usuarios: %s",
+					b.getMessage());
+			resultado = "FRACASO";
+		}
+		return resultado;
+	}
+
+}
